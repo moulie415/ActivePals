47eef17687195e794ef6b99996670f01be800b24
Henry Moule <henry.moule@dyrnan.com>
2018-01-02 02:02:17 +0000

    added react navigation

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
+import React, { Component } from "react"
+import { StyleSheet, Text, Button, View } from "react-native"
+import { StackNavigator } from "react-navigation"
 import * as firebase from "firebase"
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+})
 
-export default class App extends Component<{}> {
+ export default class App extends Component {
 
- componentDidMount() {
-
+  componentDidMount() {
    let config = {
     apiKey: "AIzaSyDIjOw0vXm7e_4JJRbwz3R787WH2xTzmBw",
     authDomain: "anyone-80c08.firebaseapp.com",
@@ -35,38 +35,18 @@ export default class App extends Component<{}> {
   }
   firebase.initializeApp(config)
 }
-  render() {
+
+  render () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        Welcome to React Native Navigation Sample!
+      </Text>
+      <Button
+        onPress={() => this.props.navigation.navigate("SecondScreen")}
+        title="Go to Second Screen"
+      />
+    </View>
+  )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+
+import React from "react"
 import { AppRegistry } from 'react-native';
 import App from './App';
+import { StackNavigator } from "react-navigation";
+import SecondScreen from "Anyone/SecondScreen";
 
-AppRegistry.registerComponent('Anyone', () => App);
+
+const reactNavigationSample = props => {
+  return <App navigation={props.navigation} />;
+};
+
+reactNavigationSample.navigationOptions = {
+  title: "Home Screen"
+};
+
+const SimpleApp = StackNavigator({
+  Home: { screen: reactNavigationSample },
+  SecondScreen: { screen: SecondScreen, title: "Second Screen" }
+});
+
+AppRegistry.registerComponent('Anyone', () => SimpleApp);
