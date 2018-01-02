4b911e5042e7f4c7ebaeaae41b23a927b6d88a51
Henry Moule <henry.moule@dyrnan.com>
2018-01-02 00:39:34 +0000

    import firebase

--- App.js
+++ App.js
@@ -12,6 +12,8 @@ import {
   View
 } from 'react-native';
 
+import * as firebase from "firebase"
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -20,6 +22,19 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component<{}> {
+
+ componentDidMount() {
+
+   let config = {
+    apiKey: "AIzaSyDIjOw0vXm7e_4JJRbwz3R787WH2xTzmBw",
+    authDomain: "anyone-80c08.firebaseapp.com",
+    databaseURL: "https://anyone-80c08.firebaseio.com",
+    projectId: "anyone-80c08",
+    storageBucket: "anyone-80c08.appspot.com",
+    messagingSenderId: "680139677816"
+  }
+  firebase.initializeApp(config)
+}
   render() {
     return (
       <View style={styles.container}>
