d00ee76a1187c9bebe8ab2b9587986bf7af486f4
Henry Moule <henry.moule@dyrnan.com>
2018-01-23 23:05:30 +0000

    sessiondetail screen and general styling

---
+++ sessions/SessionDetail.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react"
+
+import {
+	Container,
+	Card,
+	Input,
+	Picker,
+	ActionSheet,
+} from 'native-base'
+import {
+	Text,
+	View,
+	TouchableOpacity,
+} from 'react-native'
+
+
+export default class SessionDetail extends Component {
+
+
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			gender: 'All'
+		}
+
+	}
+
+	render() {
+		return (
+			<Container style={{marginHorizontal: 10}}>
+				<Card >
+					<Input 
+					style={{padding: 5, borderWidth: 1, borderColor: '#000', flex: 1, margin: 10}}
+					textAlignVertical={'top'}
+					placeholder='Title'/>
+					<Input
+					style={{padding: 5, borderWidth: 1, borderColor: '#000', flex: 3, margin: 10}}
+					placeholder='Details...'
+					multiline={true}
+					numberOfLines={7}/>
+					<View style={{flex: 2}}>
+						<Text style={{fontSize: 30, margin: 10}}>Location</Text>
+						<View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+							<Input 
+							style={{padding: 5, borderWidth: 1, borderColor: '#000', margin: 10}}
+							placeholder='Enter postcode'/>
+							<TouchableOpacity>
+								<Text style={{color: 'blue', fontSize: 20, margin: 10}}>Select on map</Text>
+							</TouchableOpacity>
+						</View>
+					</View>
+					<TouchableOpacity 
+					style={{flex: 1}}
+					onPress={()=> {
+						ActionSheet.show(
+						{
+							options: ['All', 'Male', 'Female', 'Cancel'],
+							cancelButtonIndex: 3,
+							title: "Gender?"
+						},
+						buttonIndex => {
+							switch(buttonIndex) {
+								case 0:
+									this.setState({gender: 'All'})
+									break
+								case 1:
+									this.setState({gender: 'Male'})
+									break
+								case 2:
+									this.setState({gender: 'Female'})
+									break
+							}
+						}
+						)
+					}}>
+						<Text style={{fontSize: 30, margin: 10}}>{'Gender: ' + this.state.gender}</Text>
+					</TouchableOpacity>
+				</Card>
+
+
+			</Container>
+			)
+	}
+
+}
--- sessions/SessionType.js
+++ sessions/SessionType.js
@@ -1,13 +1,14 @@
-//icons bicycle football?
-
 import React, { Component } from "react"
 
 import {
 	Icon,
-	Container
+	Container,
+	Card,
 } from 'native-base'
 import {
-	Image
+	Image,
+	TouchableOpacity,
+	Text
 } from 'react-native'
 
 
@@ -17,15 +18,36 @@ export default class SessionType extends Component {
 	constructor(props) {
 		super(props)
 
+		this.nav = this.props.navigation
+
 	}
 
 	render() {
 		return (
 			<Container>
-				<Image style={{tintColor:'red'}} source={require('../images/dumbbell.png')}/>
-				<Icon name='bicycle' style={{fontSize: 50}}/>
-				<Image style={{tintColor:'green'}} source={require('../images/running.png')}/>
-				<Image style={{tintColor:'blue'}} source={require('../images/soccer.png')}/>
+				<TouchableOpacity style={{flex: 1, marginHorizontal: 10}} onPress={()=> this.nav.navigate('SessionDetail')}>
+					<Card style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+						<Image style={{height: 50, width: 50}} source={require('../images/dumbbell.png')}/>
+						<Text style={{fontSize: 30, marginLeft: 10}}>Gym?</Text>
+					</Card>
+				</TouchableOpacity>
+				<TouchableOpacity style={{flex: 1, marginHorizontal: 10}} onPress={()=> this.nav.navigate('SessionDetail')}>
+					<Card style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+						<Image style={{height: 50, width: 50}} source={require('../images/running.png')}/>
+						<Text style={{fontSize: 30, marginLeft: 10}}>Running?</Text>
+					</Card>
+				</TouchableOpacity>
+				<TouchableOpacity style={{flex: 1, marginHorizontal: 10}} onPress={()=> this.nav.navigate('SessionDetail')}>
+					<Card style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+						<Icon name='bicycle' style={{fontSize: 50}}/>
+						<Text style={{fontSize: 30, marginLeft: 10}}>Cycling?</Text>
+					</Card>
+				</TouchableOpacity>
+				<TouchableOpacity style={{flex: 1, marginHorizontal: 10}} onPress={()=> this.nav.navigate('SessionDetail')}>
+					<Card style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+						<Text style={{fontSize: 30, marginLeft: 10}}>Custom?</Text>
+					</Card>
+				</TouchableOpacity>
 
 			</Container>
 			)
