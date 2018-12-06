27cebfeff614bc7774b70ef08353b776e7261089
Henry Moule <henry.moule@dyrnan.com>
2018-12-06 16:26:30 +0000

    hide status bar on fullscreen video

--- js/FullScreenVideo.js
+++ js/FullScreenVideo.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react"
 import VideoPlayer from 'react-native-video-controls'
+import {
+    StatusBar,
+    View
+} from 'react-native'
 
 
 class FullScreenVideo extends Component {
@@ -11,13 +15,16 @@ class FullScreenVideo extends Component {
       }
 
       render() {
-        return <VideoPlayer
+        return <View style={{flex: 1}}>
+        <StatusBar hidden />
+        <VideoPlayer
         source={{uri: this.uri}}
         disableVolume={true}
         disableFullscreen={true}
         style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
         onBack={()=> this.props.goBack()}
     />
+    </View>
       }
 }
 
