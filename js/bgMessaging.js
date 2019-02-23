02bd1ad92aa33ccad3ab37159e79d9118b6cf4c8
Henry Moule <hen10@hotmail.co.uk>
2019-02-23 13:45:06 +0000

    messaging fix

--- js/bgMessaging.js
+++ js/bgMessaging.js
@@ -1,11 +1,12 @@
-// @flow
-import firebase from 'react-native-firebase';
-// Optional flow type
+import { newNotification, updateLastMessage } from 'Anyone/js/actions/chats'
 import type { RemoteMessage } from 'react-native-firebase'
-import { showLocalNotification } from '../index'
+import { showLocalNotification, store } from '../index'
 
 export default async (message: RemoteMessage) => {
     // handle your message
+    const { dispatch } = store
+    dispatch(newNotification(message.data))
+    dispatch(updateLastMessage(message.data))
     showLocalNotification(message.data)
     return Promise.resolve();
 }
