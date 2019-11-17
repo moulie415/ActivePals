533cf7abd62bb334ac04dff6a60aca2306dcc3c0
Henry Moule <henry@care-planner.co.uk>
2019-11-17 19:46:24 +0000

    fix android build issues

---
+++ app/types/components/Button.ts
@@ -0,0 +1,9 @@
+import { StyleProp, ViewStyle, TextStyle } from "react-native";
+
+export default interface ButtonProps {
+    color?: string;
+    textColor?: string;
+    text: string;
+    style?: StyleProp<ViewStyle>;
+    textStyle?: StyleProp<TextStyle>;
+}
