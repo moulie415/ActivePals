92593c8f76c21e00d02a3a4fa4ef90eb90e113b3
Henry Moule <henry@care-planner.co.uk>
2020-01-20 18:19:51 +0000

    more code refactoring

--- app/views/sessions/SessionInfo.tsx
+++ app/views/sessions/SessionInfo.tsx
@@ -70,11 +70,9 @@ class SessionInfo extends Component {
     let host
     if (session && session.host.uid == this.props.profile.uid) {
       host = this.props.profile
-    }
-    else if (session && session.host){
+    } else if (session && session.host){
       host = this.props.friends[session.host.uid] || this.props.users[session.host.uid]
     }
-    
     let gym
     if (session && session.gym) {
       gym = this.props.places[session.gym.place_id]
@@ -87,120 +85,129 @@ class SessionInfo extends Component {
           {session ? (
             <View>
               <View style={{ marginBottom: 20 }}>
-                {gym && gym.photo ?
-                  <Image style={{height: 150, width: '100%'}}
-                resizeMode='cover'
-                source={{uri: gym.photo}} />
-                : <View style={{height: 150, backgroundColor: colors.primaryLighter}}/>}
-                <View style={{backgroundColor: '#fff', alignSelf: 'center', marginTop: -40, ...globalStyles.shadow, padding: 5}}>
+                {gym && gym.photo ? (
+                  <Image style={{ height: 150, width: '100%' }} resizeMode="cover" source={{ uri: gym.photo }} />
+                ) : (
+                  <View style={{ height: 150, backgroundColor: colors.primaryLighter }} />
+                )}
+                <View style={{ backgroundColor: '#fff', alignSelf: 'center', marginTop: -40, ...globalStyles.shadow, padding: 5 }}>
                   {getType(session.type, 80)}
                 </View>
               </View>
-            <View style={{backgroundColor: '#fff', ...globalStyles.sectionShadow}}>
-              {session && host && this.getButtons(host, session)}
-              <TouchableOpacity
-                onPress={()=> Alert.alert('Details', session.details)}
-                style={[styles.infoRowContainer, styles.rowSpaceBetween]}
-              >
-                <View >
-                  {this.renderInfoHeader('Details')}
-                  <Text numberOfLines={1} style={{color: '#999'}}>{session.details}</Text>
-                </View>
-                {this.isPrivate && <PrivateIcon/>}
-                <View>
-                  {this.renderInfoHeader('Gender')}
-                  <Text style={{color: '#999'}}>{session.gender}</Text>
+              <View style={{ backgroundColor: '#fff', ...globalStyles.sectionShadow }}>
+                {session && host && this.getButtons(host, session)}
+                <TouchableOpacity
+                  onPress={()=> Alert.alert('Details', session.details)}
+                  style={[styles.infoRowContainer, styles.rowSpaceBetween]}
+                >
+                  <View >
+                    {this.renderInfoHeader('Details')}
+                    <Text numberOfLines={1} style={{ color: '#999' }}>
+                      {session.details}
+                    </Text>
                   </View>
-              </TouchableOpacity>
-              <View style={[styles.infoRowContainer, styles.rowSpaceBetween]}>
-                <TouchableOpacity style={{flex: 4}} 
-                onPress={() => Alert.alert('Date and duration', formatDateTime(session.dateTime) + durationString(session))} >
-                  {this.renderInfoHeader('Date')}
-                  <Text numberOfLines={1} style={{color: '#999'}}>{(formatDateTime(session.dateTime))
-                    + durationString(session)}</Text>
+                  {this.isPrivate && <PrivateIcon />}
+                  <View>
+                    {this.renderInfoHeader('Gender')}
+                    <Text style={{ color: '#999' }}>{session.gender}</Text>
+                  </View>
                 </TouchableOpacity>
-                <Button onPress={() => {
-                  Alert.alert(
-                    `Add ${session.title} to calendar?`,
-                    '',
-                    [
-                      {text: 'Cancel', style: 'cancel'},
-                      {text: 'Yes', onPress: async ()=> {
-                        try {
-                          const result = await RNCalendarEvents.authorizeEventStore()
-                          if (result == 'authorized') {
-                            const calendars = await RNCalendarEvents.findCalendars()
-                            const validList = calendars.filter(calendar => calendar.allowsModifications)
-                            if (validList && validList.length > 0) {
-                              const calendarId = validList[0].id
-                              await addSessionToCalendar(calendarId, session)
-                              Alert.alert('Success', session.title + ' saved to calendar')
-                            }
-                            else {
-                              Alert.alert("Sorry", "You don't have any calendars that allow modification")
-                            }
-                          }
-                        } catch(e) {
-                          Alert.alert('Error', e.message)
-                        }
-                      }}
-                    ]
-                  )
-                }}
-                text='Add to calendar'/>
-              </View>
-              <View style={[styles.infoRowContainer, styles.rowSpaceBetween]}>
-              <TouchableOpacity onPress={()=> Alert.alert('Location', session.location.formattedAddress)}
-              style={{flex: 5}}>
-                {this.renderInfoHeader('Location')}
-                <Text numberOfLines={1} style={{color: '#999'}}>{session.location.formattedAddress}</Text>
-              </TouchableOpacity>
-              {this.props.location && <View style={{flex: 2}}>
-              <Button onPress={()=> {
-                const { lat, lng } = session.location.position
-                const options = {
-                  latitude: lat,
-                  longitude: lng,
-                  cancelText: 'Cancel',
-                  sourceLatitude: this.props.location.latitude,  
-                  sourceLongitude: this.props.location.longitude,  
-                  }
-                  this.setState({popUpVisible: true, options})
-                }}
-                text='Directions'
-                style={{alignSelf: 'flex-end'}}
-              /></View>}
-              </View>
-              {gym && <TouchableOpacity 
-              onPress={() => this.props.viewGym(gym.place_id)}
-              style={[styles.infoRowContainer, styles.userRow]}>
-              <View style={{flexDirection: 'row', alignItems: 'center', marginRight: 10}}>
-                  {gym.photo ? <Image source={{uri: gym.photo}}
-                  style={{height: 40, width: 40, borderRadius: 25}}/> :
-                  getType('gym', 40)
-                  }
-                </View>
-                <View >
-                  {this.renderInfoHeader('Gym')}
-                  <Text style={{color: '#999'}}>{gym.name}</Text>
-                </View>
-                  
-              </TouchableOpacity>}
-              {host && <TouchableOpacity 
-              onPress={() => this.handleUserPress(host.uid)}
-              style={[styles.infoRowContainer, styles.userRow, { paddingVertical: host.avatar ? 10 : 5}]}>
-              <View
-                  style={{flexDirection: 'row', alignItems: 'center', marginRight: 10}}
+                <View style={[styles.infoRowContainer, styles.rowSpaceBetween]}>
+                  <TouchableOpacity
+                    style={{ flex: 4 }}
+                    onPress={() => {
+                      Alert.alert('Date and duration', formatDateTime(session.dateTime) + durationString(session));
+                    }}
                   >
-                  {host.avatar ? <Image source={{uri: host.avatar}} style={{height: 40, width: 40, borderRadius: 25}}/> :
-                  <Icon size={50} name='md-contact'  style={{color: colors.primary}}/>}
+                    {this.renderInfoHeader('Date')}
+                    <Text numberOfLines={1} style={{ color: '#999' }}>
+                      {formatDateTime(session.dateTime) + durationString(session)}
+                    </Text>
+                  </TouchableOpacity>
+                  <Button
+                    onPress={() => {
+                      Alert.alert(
+                        `Add ${session.title} to calendar?`,
+                        '',
+                        [
+                          {text: 'Cancel', style: 'cancel'},
+                          {text: 'Yes', onPress: async ()=> {
+                            try {
+                              const result = await RNCalendarEvents.authorizeEventStore()
+                              if (result == 'authorized') {
+                                const calendars = await RNCalendarEvents.findCalendars()
+                                const validList = calendars.filter(calendar => calendar.allowsModifications)
+                                if (validList && validList.length > 0) {
+                                  const calendarId = validList[0].id
+                                  await addSessionToCalendar(calendarId, session)
+                                  Alert.alert('Success', session.title + ' saved to calendar')
+                                }
+                                else {
+                                  Alert.alert("Sorry", "You don't have any calendars that allow modification")
+                                }
+                              }
+                            } catch(e) {
+                              Alert.alert('Error', e.message)
+                            }
+                          }}
+                        ]
+                      )
+                    }}
+                    text="Add to calendar"
+                  />
                 </View>
-                <View style={{marginRight: 10}}>
-                  {this.renderInfoHeader('Host')}
-                  <Text style={{color: '#999'}}>{host.username}</Text>
+                <View style={[styles.infoRowContainer, styles.rowSpaceBetween]}>
+                <TouchableOpacity onPress={()=> Alert.alert('Location', session.location.formattedAddress)}
+                style={{flex: 5}}>
+                  {this.renderInfoHeader('Location')}
+                  <Text numberOfLines={1} style={{color: '#999'}}>{session.location.formattedAddress}</Text>
+                </TouchableOpacity>
+                {this.props.location && <View style={{flex: 2}}>
+                <Button onPress={()=> {
+                  const { lat, lng } = session.location.position
+                  const options = {
+                    latitude: lat,
+                    longitude: lng,
+                    cancelText: 'Cancel',
+                    sourceLatitude: this.props.location.latitude,  
+                    sourceLongitude: this.props.location.longitude,  
+                    }
+                    this.setState({popUpVisible: true, options})
+                  }}
+                  text='Directions'
+                  style={{alignSelf: 'flex-end'}}
+                /></View>}
                 </View>
-              </TouchableOpacity>}
-            </View>
+                {gym && <TouchableOpacity 
+                onPress={() => this.props.viewGym(gym.place_id)}
+                style={[styles.infoRowContainer, styles.userRow]}>
+                <View style={{flexDirection: 'row', alignItems: 'center', marginRight: 10}}>
+                    {gym.photo ? <Image source={{uri: gym.photo}}
+                    style={{height: 40, width: 40, borderRadius: 25}}/> :
+                    getType('gym', 40)
+                    }
+                  </View>
+                  <View >
+                    {this.renderInfoHeader('Gym')}
+                    <Text style={{color: '#999'}}>{gym.name}</Text>
+                  </View>
+                    
+                </TouchableOpacity>}
+                {host && <TouchableOpacity 
+                onPress={() => this.handleUserPress(host.uid)}
+                style={[styles.infoRowContainer, styles.userRow, { paddingVertical: host.avatar ? 10 : 5}]}>
+                <View
+                    style={{flexDirection: 'row', alignItems: 'center', marginRight: 10}}
+                    >
+                    {host.avatar ? <Image source={{uri: host.avatar}} style={{height: 40, width: 40, borderRadius: 25}}/> :
+                    <Icon size={50} name='md-contact'  style={{color: colors.primary}}/>}
+                  </View>
+                  <View style={{marginRight: 10}}>
+                    {this.renderInfoHeader('Host')}
+                    <Text style={{color: '#999'}}>{host.username}</Text>
+                  </View>
+                </TouchableOpacity>}
+              </View>
             <View style={{backgroundColor: '#fff', ...globalStyles.sectionShadow, marginTop:  20}}>
               <View style={[styles.rowSpaceBetween, {padding: 5, paddingHorizontal: 10}]}>
                 {this.renderInfoHeader('Users')}
