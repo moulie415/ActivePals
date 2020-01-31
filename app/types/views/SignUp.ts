40dc64cdc2f36eea871a5095daeb9c5f2a766606
Henry Moule <henry@care-planner.co.uk>
2020-01-31 14:47:41 +0000

    sign up improvements

---
+++ app/types/views/SignUp.ts
@@ -0,0 +1,3 @@
+export default interface SignUpProps {
+  goBack: () => void;
+}
--- app/views/SignUp.tsx
+++ app/views/SignUp.tsx
@@ -1,132 +1,82 @@
 import React, { Component } from 'react';
 import { Alert, View, ImageBackground, KeyboardAvoidingView, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { PulseIndicator } from 'react-native-indicators';
 import firebase from 'react-native-firebase';
+import { connect } from 'react-redux';
 import styles from '../styles/signUpStyles';
 import colors from '../constants/colors';
 import Header from '../components/Header/header';
-import { PulseIndicator } from 'react-native-indicators';
 import sStyles from '../styles/settingsStyles';
-const background = require('Anyone/assets/images/Running-background.jpg');
 import Button from '../components/Button';
+import { navigateBack } from '../actions/navigation';
+import { AccountType } from '../types/Profile';
+import str from '../constants/strings';
+import SignUpProps from '../types/views/SignUp';
 
-class SignUp extends Component {
-  static navigationOptions = {
-    header: null,
-  };
+const background = require('../../assets/images/Running-background.jpg');
 
+interface State {
+  spinner: boolean;
+  username?: string;
+  email?: string;
+  pass?: string;
+  confirm?: string;
+}
+class SignUp extends Component<SignUpProps, State> {
   constructor(props) {
     super(props);
-
-    this.username = '';
-    this.email = '';
-    this.pass = '';
-    this.confirm = '';
     this.state = {
       spinner: false,
     };
   }
 
-  render() {
-    return (
-      <ImageBackground style={styles.container} source={background}>
-        <Header hasBack title={'Sign up'} />
-        {this.state.spinner && (
-          <View style={sStyles.spinner}>
-            <PulseIndicator color={colors.secondary} />
-          </View>
-        )}
-        <KeyboardAvoidingView behavior="padding" style={{ justifyContent: 'center', flex: 1 }}>
-          <View style={styles.inputGrp}>
-            <Icon size={25} name="ios-person" style={{ color: '#fff', marginRight: 5 }} />
-            <TextInput
-              placeholder="Username"
-              onChangeText={u => (this.username = u)}
-              placeholderTextColor="#FFF"
-              style={styles.input}
-              autoCapitalize={'none'}
-              autoCorrect={false}
-              keyboardType={'email-address'}
-            />
-          </View>
-          <View style={styles.inputGrp}>
-            <Icon size={25} name="md-mail" style={styles.icon} />
-            <TextInput
-              placeholder="Email"
-              onChangeText={e => (this.email = e)}
-              placeholderTextColor="#FFF"
-              style={styles.input}
-              autoCapitalize={'none'}
-              autoCorrect={false}
-              keyboardType={'email-address'}
-            />
-          </View>
-          <View style={styles.inputGrp}>
-            <Icon size={25} name="md-unlock" style={styles.icon} />
-            <TextInput
-              placeholder="Password"
-              secureTextEntry
-              placeholderTextColor="#FFF"
-              onChangeText={p => (this.pass = p)}
-              style={styles.input}
-            />
-          </View>
-          <View style={styles.inputGrp}>
-            <Icon size={25} name="md-unlock" style={styles.icon} />
-            <TextInput
-              placeholder="Confirm Password"
-              secureTextEntry
-              placeholderTextColor="#FFF"
-              onChangeText={p => (this.confirm = p)}
-              style={styles.input}
-            />
-          </View>
-          <Button
-            style={{ paddingHorizontal: 20, alignSelf: 'center' }}
-            onPress={() => {
-              if (this.pass === this.confirm) {
-                this.setState({ spinner: true });
-                if (this.username && this.username.length > 5 && !str.whiteSpaceRegex.test(this.username)) {
-                  firebase
-                    .database()
-                    .ref('/usernames/' + this.username)
-                    .once('value')
-                    .then(snapshot => {
-                      if (snapshot.val()) {
-                        Alert.alert('Sorry', 'That username is already in use');
-                        this.setState({ spinner: false });
-                      } else {
-                        this.signup(this.email, this.pass);
-                      }
-                    });
-                } else {
-                  Alert.alert('Sorry', 'Username must be at least 5 characters long and cannot contain any spaces');
-                  this.setState({ spinner: false });
-                }
-              } else {
-                Alert.alert('Please try again', 'Passwords do not match');
-                this.setState({ spinner: false });
-              }
-            }}
-            text="Sign up"
-          />
-        </KeyboardAvoidingView>
-      </ImageBackground>
-    );
-  }
+  createUser = (uid, userData, token) => {
+    const { username } = this.state;
+    firebase
+      .database()
+      .ref('admins')
+      .child(uid)
+      .once('value', snapshot => {
+        const defaults = {
+          uid,
+          token,
+          accountType: snapshot.val() ? AccountType.ADMIN : AccountType.STANDARD,
+        };
+        //Alert.alert("Success", "Logged in as: " + userData.email)
+        firebase
+          .database()
+          .ref('users')
+          .child(uid)
+          .update({ ...userData, ...defaults });
+        if (username) {
+          firebase
+            .database()
+            .ref('usernames')
+            .child(username)
+            .set(uid);
+        }
+      });
+  };
+
+  static navigationOptions = {
+    header: null,
+  };
 
   async signup(email, pass) {
+    const { username } = this.state;
+    const { goBack } = this.props;
     try {
       await firebase
         .auth()
         .createUserWithEmailAndPassword(email, pass)
-        .then(({ info, user }) => {
-          const userData = { uid: user.uid, email: user.email, username: this.username };
+        .then(({ user }) => {
+          const userData = { uid: user.uid, email: user.email, username };
           this.createUser(user.uid, userData, '');
           user
             .sendEmailVerification()
             .then(() => {
-              this.props.goBack();
+              goBack();
               Alert.alert(
                 'Account created',
                 'You must now verify your email using the link we sent you before you can login'
@@ -150,38 +100,97 @@ class SignUp extends Component {
     }
   }
 
-  createUser = (uid, userData, token) => {
-    firebase
-      .database()
-      .ref('admins')
-      .child(uid)
-      .once('value', snapshot => {
-        const defaults = {
-          uid,
-          token,
-          accountType: snapshot.val() ? AccountType.ADMIN : AccountType.STANDARD,
-        };
-        //Alert.alert("Success", "Logged in as: " + userData.email)
-        firebase
-          .database()
-          .ref('users')
-          .child(uid)
-          .update({ ...userData, ...defaults });
-        if (this.username) {
-          firebase
-            .database()
-            .ref('usernames')
-            .child(this.username)
-            .set(uid);
-        }
-      });
-  };
+  render() {
+    const { username, email, pass, confirm, spinner } = this.state;
+    return (
+      <ImageBackground style={styles.container} source={background}>
+        <Header hasBack title="Sign up" />
+        {spinner && (
+          <View style={sStyles.spinner}>
+            <PulseIndicator color={colors.secondary} />
+          </View>
+        )}
+        <KeyboardAvoidingView behavior="padding" style={{ justifyContent: 'center', flex: 1 }}>
+          <View style={styles.inputGrp}>
+            <Icon size={25} name="ios-person" style={{ color: '#fff', marginRight: 5 }} />
+            <TextInput
+              placeholder="Username"
+              onChangeText={u => this.setState({ username: u })}
+              value={username}
+              placeholderTextColor="#FFF"
+              style={styles.input}
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.inputGrp}>
+            <Icon size={25} name="md-mail" style={styles.icon} />
+            <TextInput
+              placeholder="Email"
+              onChangeText={e => this.setState({ email: e })}
+              value={email}
+              placeholderTextColor="#FFF"
+              style={styles.input}
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.inputGrp}>
+            <Icon size={25} name="md-unlock" style={styles.icon} />
+            <TextInput
+              placeholder="Password"
+              secureTextEntry
+              placeholderTextColor="#FFF"
+              onChangeText={p => this.setState({ pass: p })}
+              value={pass}
+              style={styles.input}
+            />
+          </View>
+          <View style={styles.inputGrp}>
+            <Icon size={25} name="md-unlock" style={styles.icon} />
+            <TextInput
+              placeholder="Confirm Password"
+              secureTextEntry
+              placeholderTextColor="#FFF"
+              onChangeText={p => this.setState({ confirm: p })}
+              value={confirm}
+              style={styles.input}
+            />
+          </View>
+          <Button
+            style={{ paddingHorizontal: 20, alignSelf: 'center' }}
+            onPress={async () => {
+              if (pass === confirm) {
+                this.setState({ spinner: true });
+                if (username && username.length > 5 && !str.whiteSpaceRegex.test(username)) {
+                  const snapshot = await firebase
+                    .database()
+                    .ref(`/usernames/${username}`)
+                    .once('value');
+                  if (snapshot.val()) {
+                    Alert.alert('Sorry', 'That username is already in use');
+                    this.setState({ spinner: false });
+                  } else {
+                    this.signup(email, pass);
+                  }
+                } else {
+                  Alert.alert('Sorry', 'Username must be at least 5 characters long and cannot contain any spaces');
+                  this.setState({ spinner: false });
+                }
+              } else {
+                Alert.alert('Please try again', 'Passwords do not match');
+                this.setState({ spinner: false });
+              }
+            }}
+            text="Sign up"
+          />
+        </KeyboardAvoidingView>
+      </ImageBackground>
+    );
+  }
 }
-
-import { connect } from 'react-redux';
-import { navigateBack } from '../actions/navigation';
-import { AccountType } from '../types/Profile';
-import str from '../constants/strings';
 // const mapStateToProps = ({ home, settings, profile }) => ({
 // })
 
