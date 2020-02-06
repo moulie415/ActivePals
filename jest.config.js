3e751bcb843445e74c4ef7f5ef9238f5eaa1c7a5
Henry Moule <henry@care-planner.co.uk>
2020-02-06 00:02:54 +0000

    add config changes

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,22 @@
 module.exports = {
-    preset: 'react-native',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-    setupFiles: [
-      "<rootDir>/jest/setup.js"
-    ],
-    collectCoverage: true,
-    collectCoverageFrom: ['app/**/*.{ts,tsx}'],
-    coveragePathIgnorePatterns: ['app/types', 'app/styles', 'styles.ts'],
-    // setupFilesAfterEnv: [
-    //   '<rootDir>/__tests__/__mocks__/firebase.ts',
-    //   '<rootDir>/__tests__/__mocks__/@react-native-community/geolocation.ts',
-    // ],
-    transform: {
-      '^.+\\.(js)$': '<rootDir>/node_modules/babel-jest',
-      '\\.(ts|tsx)$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-    testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/', '/__tests__/fixtures/', '/__tests__/__mocks__/'],
-    transformIgnorePatterns: [
-      'node_modules/(?!(react-native|react-native-.*|react-navigation|react-navigation-.*|@synapsestudios/react-native-.*)/)',
-    ],
-    cacheDirectory: '.jest/cache',
-  };
-  
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  setupFiles: ['<rootDir>/jest/setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['app/**/*.{ts,tsx}'],
+  coveragePathIgnorePatterns: ['app/types', 'app/styles', 'styles.ts'],
+  // setupFilesAfterEnv: [
+  //   '<rootDir>/__tests__/__mocks__/firebase.ts',
+  //   '<rootDir>/__tests__/__mocks__/@react-native-community/geolocation.ts',
+  // ],
+  transform: {
+    '^.+\\.(js)$': '<rootDir>/node_modules/babel-jest',
+    '\\.(ts|tsx)$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/', '/__tests__/fixtures/', '/__tests__/__mocks__/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|react-native-.*|react-navigation|react-navigation-.*|@synapsestudios/react-native-.*)/)',
+  ],
+  cacheDirectory: '.jest/cache',
+};
