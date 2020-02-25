5ddc75cbaa33a5df432480fad736c09a5ac7978f
Henry Moule <henry@care-planner.co.uk>
2020-02-25 21:41:15 +0000

    updated firebase files and scripts

--- firebase-android.sh
+++ firebase-android.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-firebase appdistribution:distribute android/app/build/outputs/apk/release/app-release.apk --app 1:623262962635:android:538ed02554161904 --testers-file testersAndroid.txt
+firebase appdistribution:distribute android/app/build/outputs/apk/release/app-release.apk --app 1:623262962635:android:2fbe72ef452479426cdc4e --testers-file testersAndroid.txt
--- firebase-ios.sh
+++ firebase-ios.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd ios
 # Builds the app into an archive  
-xcodebuild -workspace Anyone.xcworkspace -scheme Anyone -configuration Release -archivePath "~/personal_projects/ActivePals/export/ActivePals.xcarchive" archive  
+xcodebuild -workspace ActivePals.xcworkspace -scheme ActivePals -configuration Release -archivePath "~/personal_projects/ActivePals/export/ActivePals.xcarchive" archive  
   
 # Exports the archive according to the export options specified by the plist  
 xcodebuild -exportArchive -archivePath "~/personal_projects/ActivePals/export/ActivePals.xcarchive" -exportPath "~/personal_projects/ActivePals/export" -exportOptionsPlist export.plist
@@ -9,4 +9,4 @@ xcodebuild -exportArchive -archivePath "~/personal_projects/ActivePals/export/Ac
 cd ..
 
 # upload to firebase app distribution
-firebase appdistribution:distribute export/Anyone.ipa --app 1:623262962635:ios:f2386de9b06ebad2 --testers-file testersIOS.txt
+firebase appdistribution:distribute export/ActivePals.ipa --app 1:623262962635:ios:656bf5879c8bad806cdc4e  --testers-file testersIOS.txt
