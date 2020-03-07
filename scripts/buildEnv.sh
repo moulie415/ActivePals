fa361fd5c47536eb71adb90e4f5fa6dfcc22473d
Henry Moule <henry@care-planner.co.uk>
2020-03-07 11:54:51 +0000

    script changes

--- scripts/buildEnv.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd .. && touch .env
-echo "GOOGLE_IOS_ID=${{ secrets.GOOGLE_IOS_ID }}" >> .env
-echo "GOOGLE_WEB_ID=${{ secrets.GOOGLE_WEB_ID }}" >> .env
-echo "GOOGLE_API_KEY=${{ secrets.GOOGLE_API_KEY }}" >> .env
