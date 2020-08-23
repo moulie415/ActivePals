7131488398c987281260927d78a2deb93fdcb4a7
Henry Moule <henry@care-planner.co.uk>
2020-08-23 14:01:03 +0100

    setup ui kitten

---
+++ app/App.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {ImageProps, StyleSheet} from 'react-native';
+import {
+  ApplicationProvider,
+  Button,
+  Icon,
+  IconRegistry,
+  Layout,
+  Text,
+} from '@ui-kitten/components';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import * as eva from '@eva-design/eva';
+import {ThemeContext} from './context/themeContext';
+
+const HeartIcon = (
+  props?: Partial<ImageProps>,
+): React.ReactElement<ImageProps> => <Icon {...props} name="heart" />;
+
+const App = (): React.ReactFragment => {
+  const [theme, setTheme] = React.useState('light');
+
+  const toggleTheme = () => {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(nextTheme);
+  };
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ThemeContext.Provider value={{theme, toggleTheme}}>
+        {/* @ts-ignore */}
+        <ApplicationProvider {...eva} theme={eva[theme]}>
+          <Layout style={styles.container}>
+            <Text style={styles.text} category="h1">
+              Welcome to UI Kitten 😻
+            </Text>
+            <Text style={styles.text} category="s1">
+              Start with editing App.js to configure your App
+            </Text>
+            <Text style={styles.text} appearance="hint">
+              For example, try changing theme to Dark by using eva.dark
+            </Text>
+            <Button
+              style={styles.likeButton}
+              onPress={toggleTheme}
+              accessoryLeft={HeartIcon}>
+              LIKE
+            </Button>
+          </Layout>
+        </ApplicationProvider>
+      </ThemeContext.Provider>
+    </>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+  },
+  likeButton: {
+    marginVertical: 16,
+  },
+});
