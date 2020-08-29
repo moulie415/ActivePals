623c0023bce4762011e4f0a6f273ce5226b030e2
Henry Moule <henry@care-planner.co.uk>
2020-08-29 18:45:35 +0100

    notification fixes

--- index.js
+++ index.js
@@ -3,8 +3,18 @@
  */
 
 import {AppRegistry} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 import App from './app/App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
+import {shouldNavigate, navigateFromNotif} from './app/helpers/navigation';
+
+// Register background handler
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  if (shouldNavigate(remoteMessage.data)) {
+    navigateFromNotif(remoteMessage.data);
+  }
+  console.log('Message handled in the background!', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
