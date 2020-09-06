ba3113df884aaa234461ca9ff706a6b469da696f
Henry Moule <henry@care-planner.co.uk>
2020-09-06 13:32:11 +0100

    workflow change

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -6,3 +6,11 @@ cd ios
 SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
 PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
 echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
+
+cd ..
+touch .env
+touch .env.prod
+echo "GOOGLE_IOS_ID=${{ secrets.GOOGLE_IOS_ID }}" >> .env.prod
+echo "GOOGLE_WEB_ID=${{ secrets.GOOGLE_WEB_ID }}" >> .env.prod
+echo "GOOGLE_API_KEY=${{ secrets.GOOGLE_API_KEY }}" >> .env.prod
+cd scripts && ENV=production ./envscript.sh
