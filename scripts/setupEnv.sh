e6af0b56d5f9708dd5099cf238e45d84295a865d
Henry Moule <henry@care-planner.co.uk>
2020-11-08 16:59:36 +0000

    test changes

--- scripts/setupEnv.sh
+++ scripts/setupEnv.sh
@@ -2,6 +2,7 @@ echo "GOOGLE_IOS_ID=$GOOGLE_IOS_ID" >> .env
 echo "GOOGLE_WEB_ID=$GOOGLE_WEB_ID" >> .env
 echo "GOOGLE_API_KEY_ANDROID=$GOOGLE_API_KEY_ANDROID" >> .env
 echo "GOOGLE_API_KEY_IOS=$GOOGLE_API_KEY_IOS" >> .env
+cat .env
 cd android/app
 echo $GOOGLE_SERVICES_JSON >> google-services.json
 cd ..
@@ -9,3 +10,5 @@ echo "MYAPP_RELEASE_KEY_ALIAS=$MYAPP_RELEASE_KEY_ALIAS" >> gradle.properties
 echo "MYAPP_RELEASE_STORE_FILE=$MYAPP_RELEASE_STORE_FILE" >> gradle.properties
 echo "MYAPP_RELEASE_STORE_PASSWORD=$MYAPP_RELEASE_STORE_PASSWORD" >> gradle.properties
 echo "MYAPP_RELEASE_KEY_PASSWORD=$MYAPP_RELEASE_KEY_PASSWORD" >> gradle.properties
+ls
+cat gradle.properties
