44cf843d06c033ffcb0013a75e691fecb934b37e
Henry Moule <henry@care-planner.co.uk>
2020-11-08 16:37:46 +0000

    script fix

--- scripts/setupEnv.sh
+++ scripts/setupEnv.sh
@@ -1,11 +1,11 @@
-echo "GOOGLE_IOS_ID=${{ secrets.GOOGLE_IOS_ID }}" >> .env
-echo "GOOGLE_WEB_ID=${{ secrets.GOOGLE_WEB_ID }}" >> .env
-echo "GOOGLE_API_KEY_ANDROID=${{ secrets.GOOGLE_API_KEY_ANDROID }}" >> .env
-echo "GOOGLE_API_KEY_IOS=${{ secrets.GOOGLE_API_KEY_IOS }}" >> .env
+echo "GOOGLE_IOS_ID=$GOOGLE_IOS_ID" >> .env
+echo "GOOGLE_WEB_ID=$GOOGLE_WEB_ID" >> .env
+echo "GOOGLE_API_KEY_ANDROID=$GOOGLE_API_KEY_ANDROID" >> .env
+echo "GOOGLE_API_KEY_IOS=$GOOGLE_API_KEY_IOS" >> .env
 cd android/app
-echo ${{ secrets.GOOGLE_SERVICES_JSON }} >> google-services.json
+echo $GOOGLE_SERVICES_JSON >> google-services.json
 cd ..
-echo "MYAPP_RELEASE_KEY_ALIAS=${{ secrets.MYAPP_RELEASE_KEY_ALIAS }}" >> gradle.properties
-echo "MYAPP_RELEASE_STORE_FILE=${{ secrets.MYAPP_RELEASE_STORE_FILE }}" >> gradle.properties
-echo "MYAPP_RELEASE_STORE_PASSWORD=${{ secrets.MYAPP_RELEASE_STORE_PASSWORD }}" >> gradle.properties
-echo "MYAPP_RELEASE_KEY_PASSWORD=${{ secrets.MYAPP_RELEASE_KEY_PASSWORD }}" >> gradle.properties
+echo "MYAPP_RELEASE_KEY_ALIAS=$MYAPP_RELEASE_KEY_ALIAS" >> gradle.properties
+echo "MYAPP_RELEASE_STORE_FILE=$MYAPP_RELEASE_STORE_FILE" >> gradle.properties
+echo "MYAPP_RELEASE_STORE_PASSWORD=$MYAPP_RELEASE_STORE_PASSWORD" >> gradle.properties
+echo "MYAPP_RELEASE_KEY_PASSWORD=$MYAPP_RELEASE_KEY_PASSWORD" >> gradle.properties
